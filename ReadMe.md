f53b81bbe769f28ea1f5eef5b13d4aa6524d0848
akhilaYedla <ky.akhila@gmail.com>
2024-08-19 17:19:45 +1000

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+The main scirpts are in signUp.spec.js
+
+To run tests go to terminal and npx playwright test
+
+You need to install playwright to execute this.
---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+// playwright.config.js
+module.exports = {
+  timeout: 30000,
+  retries: 1,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+  ],
+};
